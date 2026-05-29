da650a91a30034d4fb9319bf8846cae28bd2db72
MustikaJr <mustika2008jr@gmail.com>
2026-05-29 16:46:35 +0700

    update error

--- fungsi.js
+++ fungsi.js
@@ -1,20 +1,15 @@
-// 1. Function Declaration (Cara standar membuat fungsi)
+
 function tampilkanBuku(judul, penulis) {
     return `${judul} oleh ${penulis}`;
 }
 console.log(tampilkanBuku("Laskar Pelangi", "Andrea Hirata"));
 
-
-// 2. Arrow Function (Cara singkat/modern menulis fungsi)
 const hitungTotal = (harga, jumlah) => harga * jumlah;
-console.log(hitungTotal(75000, 3)); // Output: 225000
+console.log(hitungTotal(75000, 3)); 
 
-
-// 3. Default Parameter (Menyiapkan nilai cadangan jika data kosong)
 function infoBuku(judul = "Tidak diketahui") {
     return `Buku: ${judul}`;
 }
 
-// Menguji fungsi default parameter
-console.log(infoBuku());                  // Output: Buku: Tidak diketahui
-console.log(infoBuku("Sang Pemimpi"));    // Output: Buku: Sang Pemimpi
+console.log(infoBuku());                  
+console.log(infoBuku("Sang Pemimpi"));    
